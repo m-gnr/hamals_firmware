9c93538cd369fac03e0d60566862ed002f7bebdc
m-gnr <m_gnr@icloud.com>
2026-02-25 16:30:48 +0300

    Moved right encoder from D9 to D0 to avoid RGB LED interference. Updated IMU interrupt pin to D1 for stability.

--- src/config/config_pins.h
+++ src/config/config_pins.h
@@ -3,27 +3,26 @@
 
 // -------------------- Encoder Pins ---------------------
 // Left wheel encoder
-#define ENC_L_A   D13   // D13 - interrupt capable - Channel A
-#define ENC_L_B   D14   // D14 - interrupt capable - Channel B
+#define ENC_L_A   D13   // GPIO3  - interrupt capable - Channel A
+#define ENC_L_B   D14   // GPIO8  - interrupt capable - Channel B
 
 // Right wheel encoder
-#define ENC_R_A   D8    // D8  - interrupt capable - Channel A
-#define ENC_R_B   D9    // D9  - interrupt capable - Channel B
+#define ENC_R_A   D12   // GPIO10 - interrupt capable - Channel A
+#define ENC_R_B   D0    // GPIO1  - interrupt capable - Channel B
 
 // -------------------- Motor Driver Pins ----------------
 // Left motor driver
-#define MOTOR_L_IN1  A1
-#define MOTOR_L_IN2  A0
+#define MOTOR_L_IN1  A1  // GPIO5
+#define MOTOR_L_IN2  A0  // GPIO4
 
 // Right motor driver
-#define MOTOR_R_IN1  A2
-#define MOTOR_R_IN2  A3
+#define MOTOR_R_IN1  A2  // GPIO6
+#define MOTOR_R_IN2  A3  // GPIO7
 
 // -------------------- IMU (BNO085 - SPI) ----------------
-// SPI interface
-#define IMU_CS    D4    // Chip Select
-#define IMU_INT   D0    // Data Ready Interrupt (TODO: move to D15/D16/D17)
-#define IMU_RST   D12   // Reset
+#define IMU_CS    D4    // GPIO42 - Chip Select
+#define IMU_INT   D1    // GPIO2  - Data Ready Interrupt
+#define IMU_RST   D8    // GPIO38 - Reset
 
 // -------------------- Serial ---------------------------
 #define SERIAL_BAUDRATE 115200
